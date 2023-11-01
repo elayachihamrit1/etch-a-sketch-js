bbf5fbd6ca072d5eb12e6d874e4c02ee80ae40cf
elayachihamritoff <elayachihamritoff@gmail.com>
2023-11-01 22:40:02 +0100

    Add drawing functions

---
+++ script.js
@@ -0,0 +1,36 @@
+	
+
+	const drawBoard = document.querySelector('.board');
+	const pen = document.querySelector('.pen');
+	const eraser = document.querySelector('.eraser');
+	const reset = document.querySelector('.reset');
+	const color = '#0DABDD';
+
+
+	createGrid(32);
+
+
+
+	function createGrid(size) {
+		
+
+		for(i = 0 ; i < size * size; i++) {
+			
+			let cell = document.createElement('div');
+
+			drawBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`; 
+			drawBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`; 
+			cell.classList.add('cell');
+			drawBoard.append(cell);
+		
+			cell.addEventListener('click', draw);
+		}
+
+	}
+
+
+
+	function draw(e) {
+		e.target.style.backgroundColor = `${color}`;
+	}	
+
